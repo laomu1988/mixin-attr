45a281dfe3ef3843def0315e2c3ee82e605de7b4
muzhilong <laomu1988@qq.com>
2017-01-20 15:00:33 +0800

    删除loads依赖；整理文档

--- index.js
+++ index.js
@@ -2,31 +2,31 @@
  * 混合对象属性
  * */
 'use strict';
-var _ = require('lodash');
 function mixin_object(obj, plainObject) {
     // console.log('mixin:', obj, plainObject);
-    if (!_.isPlainObject(plainObject))return obj;
+    if (!isObject(plainObject) || !isObject(obj)) return obj;
     for (var attr in plainObject) {
         var val = plainObject[attr];
-        attr = (attr + '').trim();
-        var attrs = attr.split('.');
+        var attrs = (attr + '').trim().split('.');
         var now = obj;
         if (attrs.length > 1) {
-            for (let i = 0; i < attrs.length - 1; i++) {
-                let att = attrs[i];
-                if (!_.isPlainObject(now[att])) now[att] = {};
+            for (var i = 0; i < attrs.length - 1; i++) {
+                var att = attrs[i];
+                if (!isObject(now[att])) now[att] = {};
                 now = now[att];
             }
         }
-        var lastAttr = attrs[attrs.length - 1];
-        if (!_.isPlainObject(val) || !_.isPlainObject(now[lastAttr])) {
-            // console.log('setattrr:', attrs[attrs.length - 1], val);
-            now[lastAttr] = val;
+        attr = attrs[attrs.length - 1];
+        if (!isObject(val) || !isObject(now[attr])) {
+            now[attr] = val;
         } else {
-            mixin_object(now[lastAttr], val);
+            mixin_object(now[attr], val);
         }
     }
     return obj;
+}
+function isObject(obj) {
+    return typeof obj === 'object';
 }
 
 module.exports = mixin_object;
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
 # 混合对象属性
-   混合两个对象的属性,假如属性名不重复则不会被覆盖
+ 混合两个对象的属性,假如属性名不重复则不会被覆盖
+ 例如对象{a:{b:1}}和{a:{c:1}}混合后将变成{a:{b:1,c:1}}
 
+## install
+```
+npm install mixin-object --save
+```
+
+## usage
+```
+var mixin = require('mixin-object');
+
+var target = mixin(
+    {a: {b: 1}},
+    {a: {c: 1}}
+);
+
+console.log(JSON.stringify(target));
+// 将输出: {"a":{"b":1,"c":1}}
+
+```
